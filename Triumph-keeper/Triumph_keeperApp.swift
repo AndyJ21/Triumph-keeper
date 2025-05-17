63beeb16989f182f2ebe1c37d583e5e25ce9ec95
Andy Samayoa <huehuecho2121@gmail.com>
2025-05-17 13:20:18 -0700

    Initial Commit

---
+++ Triumph-keeper/Triumph_keeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Triumph_keeperApp.swift
+//  Triumph-keeper
+//
+//  Created by Andy on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Triumph_keeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
